c752ec1cb46e616661b734ae0a7949651c7d6ede
1712916 <kieuphong2840@gmail.com>
2020-11-11 21:03:24 +0700

    Thêm các màn hình navigate trong Browse, và màn hình kết quả Search

--- app/lib/search/search-result.dart
+++ app/lib/search/search-result.dart
@@ -1,5 +1,6 @@
 import 'package:app/browse/paths/path-item.dart';
 import 'package:app/browse/topauthors/author-item.dart';
+import 'package:app/ulti/app-color.dart';
 import 'package:app/ulti/data.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
@@ -16,11 +17,14 @@ class _SearchResultState extends State<SearchResult>
   List<Author> _authors = Authors;
 
   TabController _tabController;
+
+  String dropdownValue = 'Relevance';
   @override
   void initState() {
     super.initState();
     _tabController = new TabController(vsync: this, length: 4, initialIndex: 0);
   }
+
   @override
   void dispose() {
     _tabController.dispose();
@@ -35,9 +39,7 @@ class _SearchResultState extends State<SearchResult>
           mainAxisSize: MainAxisSize.min,
           children: <Widget>[
             Container(
-              child: TabBar(
-                  controller: _tabController,
-                  tabs: [
+              child: TabBar(controller: _tabController, tabs: [
                 Tab(
                   child: Text(
                     'ALL',
@@ -51,16 +53,13 @@ class _SearchResultState extends State<SearchResult>
             Expanded(
               //Add this to give height
 
-              child: TabBarView(
-                  controller: _tabController,
-                  children: [
+              child: TabBarView(controller: _tabController, children: [
                 Container(
                   child: SingleChildScrollView(
                     child: Column(
                       children: [
                         Padding(
-                            padding:
-                                const EdgeInsets.symmetric(horizontal: 16),
+                            padding: const EdgeInsets.symmetric(horizontal: 16),
                             child: Column(children: [
                               Row(
                                 mainAxisAlignment:
@@ -70,68 +69,199 @@ class _SearchResultState extends State<SearchResult>
                                   TextButton(
                                       onPressed: () {
                                         _tabController.animateTo(1);
-
                                       },
                                       child:
                                           Text('${_courses.length} Results')),
                                 ],
                               ),
                               buildCourseItem(_courses, 4),
-
                             ])),
                         Padding(
                           padding: const EdgeInsets.symmetric(horizontal: 16),
                           child: Column(
                             children: [
                               Row(
-                                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                                mainAxisAlignment:
+                                    MainAxisAlignment.spaceBetween,
                                 children: [
                                   Text('Paths'),
-                                  TextButton(onPressed: () {
-                                    _tabController.animateTo(2);
-
-                                  }, child: Text('${_paths.length} Results')),
+                                  TextButton(
+                                      onPressed: () {
+                                        _tabController.animateTo(2);
+                                      },
+                                      child: Text('${_paths.length} Results')),
                                 ],
                               ),
                               buildPathItem(_paths, 4),
                             ],
                           ),
                         ),
-
                         Padding(
                           padding: const EdgeInsets.symmetric(horizontal: 16),
                           child: Column(
                             children: [
                               Row(
-                                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                                mainAxisAlignment:
+                                    MainAxisAlignment.spaceBetween,
                                 children: [
                                   Text('Authors'),
-                                  TextButton(onPressed: () {
-                                    _tabController.animateTo(3);
-                                  }, child: Text('${_authors.length} Results')),
+                                  TextButton(
+                                      onPressed: () {
+                                        _tabController.animateTo(3);
+                                      },
+                                      child:
+                                          Text('${_authors.length} Results')),
                                 ],
                               ),
                               buildAuthorItem(_authors, 4),
                             ],
                           ),
                         ),
-
                       ],
                     ),
                   ),
                 ),
                 Container(
-                  child: Column(
-                    children: [
-                      Text("Articles Body"),
-                    ],
+                  child: Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 16),
+                    child: Column(
+                      children: [
+                        SizedBox(
+                          height: 32,
+                        ),
+                        GestureDetector(
+                          onTap: () {
+                            showDialog(context: context,
+                            builder: (BuildContext context){
+                              return AlertDialog(
+
+                                backgroundColor: AppColors.backgroundItemTypeA,
+                                title: Text('hello'),
+                                content: Container(
+                                  height: 180,
+                                  child: Column(
+                                    children: [
+                                      CheckboxListTile(
+
+                                           title: Text('Hello'),
+                                          value:false, onChanged: (val) {
+
+                                      }),
+                                      CheckboxListTile(
+                                          title: Text('Hello'),
+
+                                          value:true, onChanged: (val) {
+
+                                      }),
+
+                                      CheckboxListTile(
+                                          title: Text('Hello'),
+                                          value:true, onChanged: (val) {
+
+                                      }),
+
+
+                                    ],
+                                  ),
+                                ),
+                                actions: [
+                                  TextButton(onPressed: (){
+                                    Navigator.of(context).pop();
+                                  }, child: Text('CANCEL')),
+                                  TextButton(onPressed: (){}, child: Text('APPLY')),
+
+                                ],
+                              );
+                            });
+                          },
+                          child: Row(
+                            children: [
+                              Text('Skill Levels '),
+                              Icon(Icons.arrow_drop_down_outlined)
+                            ],
+                          ),
+                        ),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            Text('${_courses.length} Results'),
+                            DropdownButton(
+                              value: dropdownValue,
+                              items: <String>[
+                                'Relevance',
+                                'Newest'
+                              ].map<DropdownMenuItem<String>>((String value) {
+                                return DropdownMenuItem<String>(
+                                  value: value,
+                                  child: Text(value),
+                                );
+                              }).toList(),
+                              onChanged: (String newValue) {
+                                setState(() {
+                                  dropdownValue = newValue;
+                                });
+                              },
+                            )
+                          ],
+                        ),
+                        Expanded(
+                          child: ListView(
+                            children: [
+                              SingleChildScrollView(
+                                  child: buildCourseItem(
+                                      _courses, _courses.length))
+                            ],
+                          ),
+                        )
+                      ],
+                    ),
                   ),
                 ),
                 Container(
-                  child: Text("Articles Body"),
+                  child: Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 16),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        SizedBox(
+                          height: 16,
+                        ),
+                        Text('${_paths.length} Results'),
+                        SizedBox(
+                          height: 16,
+                        ),
+                        Expanded(
+                            child: ListView(
+                          children: [
+                            buildPathItem(_paths, _paths.length),
+                          ],
+                        ))
+                      ],
+                    ),
+                  ),
                 ),
                 Container(
-                  child: Text("Articles Body"),
+                  child: Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 16),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        SizedBox(
+                          height: 16,
+                        ),
+                        Text('${_authors.length} Results'),
+                        SizedBox(
+                          height: 16,
+                        ),
+                        Expanded(
+                            child: ListView(
+                          children: [
+                            buildAuthorItem(_authors, _authors.length),
+                          ],
+                        ))
+                      ],
+                    ),
+                  ),
                 ),
               ]),
             ),
@@ -142,7 +272,7 @@ class _SearchResultState extends State<SearchResult>
   }
 }
 
- Widget buildCourseItem(List<Course> courses, int length) {
+Widget buildCourseItem(List<Course> courses, int length) {
   List<Widget> list = [];
 
   for (var i = 0; i < courses.length && i < length; i++) {
@@ -158,9 +288,8 @@ class _SearchResultState extends State<SearchResult>
   );
 }
 
- Widget buildPathItem(List<Path> paths, int length) {
+Widget buildPathItem(List<Path> paths, int length) {
   List<Widget> list = [];
-
 
   for (var i = 0; i < paths.length && i < length; i++) {
     list.add(PathItemB(path: paths[i]));
@@ -176,14 +305,15 @@ class _SearchResultState extends State<SearchResult>
 Widget buildAuthorItem(List<Author> author, int length) {
   List<Widget> list = [];
 
-
   for (var i = 0; i < author.length && i < length; i++) {
     list.add(AuthorItemB(author[i]));
     list.add(Divider(
       color: Colors.grey,
     ));
   }
-  return Column(children:  list,);
+  return Column(
+    children: list,
+  );
 }
 
 class ItemFactory {
